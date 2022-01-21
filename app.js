a580c37cb345aa834176d3d1521879f1d416c267
aymeric <aymeric.baptiste@gmail.com>
2022-01-21 21:47:33 +0100

    Modify header for support Bearer Token

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ db.mongoose
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*" ); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Headers, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+    res.header("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE, OPTIONS");
     next();
 });
 
